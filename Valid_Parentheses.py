8e05ce421b22a30631d084749cfe7d56f04bbf54
eulersformula <gaofeng.fdu@gmail.com>
2016-02-10 12:22:03 -0500

    Update Valid_Parentheses.py

--- Valid_Parentheses.py
+++ Valid_Parentheses.py
@@ -1,1 +1,28 @@
+#Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 
+#Example
+#The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+
+#Method 1: Keypoint: (1) the first appearace of some right parenthese must have its corresponding left parenthese on its left; (2) use stack to delete last input element
+
+class Solution:
+    # @param {string} s A string
+    # @return {boolean} whether the string is a valid parentheses
+    def isValidParentheses(self, s):
+        if len(s) == 0:
+            return True
+        leftParentheses = ['(', '[', '{']
+        rightParentheses = [')', ']', '}']
+        stack = []
+        for p in s:
+          if p in leftParentheses:
+            stack.append(p)
+          else:
+            if len(stack) == 0:
+              return False
+            left = stack.pop()
+            if not (p == ')' and left == '(' or p == ']' and left == '[' or p == '}' and left == '{'):
+              return False
+        if len(stack) != 0:
+          return False
+        return True
