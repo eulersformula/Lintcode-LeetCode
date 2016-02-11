85617677bf5781a56e53b7cf9c5283f5aec14db9
eulersformula <gaofeng.fdu@gmail.com>
2016-02-11 17:58:38 -0500

    Create Trainling_Zeros.py

---
+++ Trainling_Zeros.py
@@ -0,0 +1,12 @@
+#Write an algorithm which computes the number of trailing zeros in n factorial.
+
+#Example
+#11! = 39916800, so the out should be 2.
+
+#Challenge
+#O(log N) time
+
+#Earth people all know this: count the number of 5's in the factors of 1,2,3,...,n.
+
+
+
