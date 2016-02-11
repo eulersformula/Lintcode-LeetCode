7f76f5b93631665a410b47ced5aef98acd7f4f36
eulersformula <gaofeng.fdu@gmail.com>
2016-02-11 16:45:37 -0500

    Create Reverse_Linked_List.py

---
+++ Reverse_Linked_List.py
@@ -0,0 +1,24 @@
+#Reverse a linked list.
+
+#Example
+#For linked list 1->2->3, the reversed linked list is 3->2->1
+
+#Challenge
+#Reverse it in-place and in one-pass
+
+"""
+Definition of ListNode
+
+class ListNode(object):
+
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+class Solution:
+    """
+    @param head: The first node of the linked list.
+    @return: You should return the head of the reversed linked list. 
+                  Reverse it in-place.
+    """
+    def reverse(self, head):
