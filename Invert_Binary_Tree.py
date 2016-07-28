6266ef1439df179a1ba611098bbb7d7dc0abc71e
eulersformula <gaofeng.fdu@gmail.com>
2016-07-28 11:50:57 -0400

    Create Invert_Binary_Tree.py

---
+++ Invert_Binary_Tree.py
@@ -0,0 +1,47 @@
+#######################################################################################################################################
+# Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so fuck off. #
+#######################################################################################################################################
+
+#Invert a binary tree.
+#Example
+#  1         1
+# / \       / \
+#2   3  => 3   2
+#   /       \
+#  4         4
+
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+#Method 1: Recursion
+class Solution:
+    # @param root: a TreeNode, the root of the binary tree
+    # @return: nothing
+    def invertBinaryTree(self, root):
+        if root != None:
+            root.left, root.right = root.right, root.left
+            self.invertBinaryTree(root.left)
+            self.invertBinaryTree(root.right)
+
+#Method 2: Iterative (How to assess evrery node in a tree without recursion? Store all nodes in a list!)
+class Solution:
+    # @param root: a TreeNode, the root of the binary tree
+    # @return: nothing
+    def invertBinaryTree(self, root):
+        allNodes = []
+        if root != None:
+          allNodes.append(root)
+        while allNodes != []:
+          node = allNodes.pop(0)
+          if node.left != None:
+            allNodes.append(node.left)
+          if node.right != None:
+            allNodes.append(node.right)
+          node.left, node.right = node.right, node.left
+        return root
+          
