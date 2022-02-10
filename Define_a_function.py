0c76747e3dd1fc08151e65bf41d46c50dfa4ed23
Rinnegan <gaofeng.fdu@gmail.com>
2022-02-09 23:13:10 -0800

    Create Define_a_function.py

---
+++ Define_a_function.py
@@ -0,0 +1,13 @@
+# Lintcode 2960//Naive
+# Description
+# You are asked to define a function return_hello that returns a string 'hello'.
+
+# Example
+# The evaluation machine will execute your code via python main.py. There is no input data for this question, so please define a function in solution.py named my_name that returns a string. We will execute your code in main.py, and you can see the exact execution steps in main.py.
+# Output.
+# hello
+
+# Notice how to define a function without an argument
+
+def my_name():
+    return 'hello'
