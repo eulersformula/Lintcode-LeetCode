fef6cb9745f931dfb14daff74def50c8ee38f397
Rinnegan <gaofeng.fdu@gmail.com>
2022-05-18 20:32:15 -0700

    Create Max_Stack.py

---
+++ Max_Stack.py
@@ -0,0 +1,94 @@
+# Lintcode 859//Hard//LinkedIn
+
+# Description
+# Design a max stack that supports push, pop, top, peekMax and popMax.
+
+# push(x) -- Push element x onto stack.
+# pop() -- Remove the element on top of the stack and return it.
+# top() -- Get the element on the top.
+# peekMax() -- Retrieve the maximum element in the stack.
+# popMax() -- Retrieve the maximum element in the stack, and remove it. If you find more than one maximum elements, only remove the top-most one.
+# -1e7 <= x <= 1e7
+# Number of operations won't exceed 10000.
+# The last four operations won't be called when stack is empty.
+# Example
+# Input:
+# push(5)
+# push(1)
+# push(5)
+# top()
+# popMax()
+# top()
+# peekMax()
+# pop()
+# top()
+# Output:
+# 5
+# 5
+# 1
+# 5
+# 1
+# 5
+
+class MaxStack:
+    def __init__(self):
+        # do intialization if necessary
+        self.stack = list()
+        self.length = 0
+    
+    """
+    @param: number: An integer
+    @return: nothing
+    """
+    def push(self, x):
+        # write your code here
+        if self.length == 0 or self.stack[-1][1] <= x:
+            cur_max, max_idx = x, self.length
+        else:
+            cur_max, max_idx = self.stack[-1][1], self.stack[-1][2]
+        self.stack.append([x, cur_max, max_idx])
+        self.length += 1
+
+    """
+    @return: An integer
+    """
+    def pop(self):
+        # write your code here
+        self.length -= 1
+        return self.stack.pop()[0] # 一开始此处忘了return
+
+    """
+    @return: An integer
+    """
+    def top(self):
+        # write your code here
+        return self.stack[-1][0]
+
+    """
+    @return: An integer
+    """
+    def peekMax(self):
+        # write your code here
+        return self.stack[-1][1]
+
+    """
+    @return: An integer
+    """
+    def popMax(self):
+        # write your code here
+        max_idx = self.stack[-1][2]
+        prev_element = self.stack[max_idx-1] if max_idx > 0 else None # if None, first element is popped
+        for idx in range(max_idx+1, self.length):
+            if prev_element is None:
+                self.stack[idx][1] = self.stack[idx][0]
+                self.stack[idx][2] = 0
+            elif self.stack[idx][0] >= prev_element[1]:
+                self.stack[idx][1] = self.stack[idx][0]
+                self.stack[idx][2] = idx - 1
+            else:
+                self.stack[idx][1] = prev_element[1]
+                self.stack[idx][2] = prev_element[2]
+            prev_element = self.stack[idx]
+        self.length -= 1
+        return self.stack.pop(max_idx)[0]
+        
