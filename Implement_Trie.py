a6414a76e2fa00646b5f0997eee0e6b8cd53eea3
Rinnegan <gaofeng.fdu@gmail.com>
2022-05-25 23:29:25 -0700

    Update Implement_Trie.py

--- Implement_Trie.py
+++ Implement_Trie.py
@@ -1,3 +1,6 @@
+# Lintcode 442//Medium
+# Leetcode 208//Medium
+
 #Implement a trie with insert, search, and startsWith methods.
 #Notice: You may assume that all inputs are consist of lowercase letters a-z.
 #Example:
@@ -62,3 +65,57 @@ class Trie:
             return False
         node = node.children[p]
     return True
+ 
+# 二刷code（Leetcode）
+class TreeNode:
+    def __init__(self, val=''):
+        self.val = val
+        self.is_word = False
+        self.children = dict()
+
+class Trie:
+
+    def __init__(self):
+        self.root = TreeNode() # initialize with empty string as root
+        
+    def insert(self, word: str) -> None:
+        if len(word) == 0:
+            self.root.is_word = True
+        else:
+            idx, node = 0, self.root
+            while idx < len(word):
+                letter = word[idx]
+                if letter not in node.children:
+                    node.children[letter] = TreeNode(letter)
+                node = node.children[letter]
+                if idx == len(word) - 1:
+                    node.is_word = True
+                idx += 1   
+
+    def search(self, word: str) -> bool:
+        if len(word) == 0:
+            return self.root.is_word
+        idx, node = 0, self.root
+        while idx < len(word):
+            letter = word[idx]
+            if letter not in node.children:
+                return False
+            node = node.children[letter]
+            if idx == len(word) - 1 and node.is_word:
+                return True
+            idx += 1
+        return False
+
+    def startsWith(self, prefix: str) -> bool:
+        if len(prefix) == 0:
+            if len(self.root.children) > 0:
+                return True
+            return self.root.is_word
+        idx, node = 0, self.root
+        while idx < len(prefix):
+            letter = prefix[idx]
+            if letter not in node.children:
+                return False
+            node = node.children[letter]
+            idx += 1
+        return True
