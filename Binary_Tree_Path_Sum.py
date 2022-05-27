28c250332da47b863adc5c669f7c86a68ddab0bc
Rinnegan <gaofeng.fdu@gmail.com>
2022-05-26 22:02:24 -0700

    Update Binary_Tree_Path_Sum.py

--- Binary_Tree_Path_Sum.py
+++ Binary_Tree_Path_Sum.py
@@ -52,6 +52,7 @@ class TreeNode:
         self.left, self.right = None, None
 """
 
+# Solution 1: Recursion
 class Solution:
     """
     @param root: the root of binary tree
@@ -75,4 +76,45 @@ class Solution:
         right_path = self.binary_tree_path_sum(root.right, target-root.val)
         if len(right_path) > 0:
             res += [[root.val] + p for p in right_path]
+        return res
+
+# Solution 2: DFS
+class Solution:
+    """
+    @param root: the root of binary tree
+    @param target: An integer
+    @return: all valid paths
+             we will sort your return value in output
+    """
+    def binary_tree_path_sum(self, root: TreeNode, target: int) -> List[List[int]]:
+        # write your code here
+        # 注意审题：必须是从root到leaf node的path
+        # Questions to ask:
+        # 1. What is the range of values in the binary tree
+        # 通过Testing case得知node.val可以小于0
+        if root is None:
+            return []
+        from collections import deque
+        stack = deque([root])
+        path = deque([[root.val]])
+        path_sum = deque([root.val])
+        res = []
+        while len(stack) > 0:
+            node = stack.popleft()
+            p = path.popleft()
+            s = path_sum.popleft()
+            print(p, s, node.val)
+            if node.left is None and node.right is None: # leaf node 
+                if s == target:
+                    res.append(p)
+            else: # not leaf node
+                if node.right is not None:
+                    stack.append(node.right)
+                    path.append(p+[node.right.val]) # list.append() 返回值为None
+                    path_sum.append(s+node.right.val)
+                if node.left is not None:
+                    print('left not none')
+                    stack.append(node.left)
+                    path.append(p+[node.left.val])
+                    path_sum.append(s+node.left.val)
         return res
