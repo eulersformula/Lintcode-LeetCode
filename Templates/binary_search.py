041264d1a8fd00bce59a4ba34364ba017f830d1a
Rinnegan <gaofeng.fdu@gmail.com>
2022-06-07 14:30:49 -0700

    Update binary_search.py

--- Templates/binary_search.py
+++ Templates/binary_search.py
@@ -1,8 +1,33 @@
+# 左闭右开写法
 def binary_search(arr, target):
-  if len(arr) == 0:
-    return None
-  st, ed = 0, len(arr)
+  st, ed = 0, len(arr) # 退出条件st == ed
   while st < ed:
+    mid = st + (ed - st) // 2
+    if arr[mid] == target:
+      return mid
+    if arr[mid] < target:
+      st = mid + 1
+    else:
+      ed = mid
+  return -1
+
+#左闭右闭写法
+def binary_search(arr, target):
+  st, ed = 0, len(arr)-1
+  while st <= ed: # 退出条件st > ed
+    mid = (st + ed) // 2
+    if arr[mid] == target:
+      return mid
+    if arr[mid] < target:
+      st = mid + 1
+    else:
+      ed = mid - 1
+  return -1
+
+# 相邻写法
+def binary_search(arr, target):
+  st, ed = 0, len(arr)-1 # 退出条件st == ed - 1
+  while st < ed - 1:
     mid = (st + ed) // 2
     if arr[mid] == target:
       return mid
@@ -10,4 +35,4 @@ def binary_search(arr, target):
       st = mid
     else:
       ed = mid
-  return -1
+return -1
