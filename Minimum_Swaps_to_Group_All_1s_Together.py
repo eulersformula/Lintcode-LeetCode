5f8ba4b16accd2890c1c3237405411c78d0b567f
Rinnegan <gaofeng.fdu@gmail.com>
2022-06-20 17:54:57 -0700

    Create Minimum_Swaps_to_Group_All_1s_Together.py

---
+++ Minimum_Swaps_to_Group_All_1s_Together.py
@@ -0,0 +1,59 @@
+# Leetcode 1151//Medium//Expedia
+# Description
+# Given a binary array data, return the minimum number of swaps required to group all 1’s present in the array together in any place in the array.
+# Example 1:
+# Input: [1,0,1,0,1]
+# Output: 1
+# Explanation:
+# There are 3 ways to group all 1’s together:
+
+# [1,1,1,0,0] using 1 swap.
+
+# [0,1,1,1,0] using 2 swaps.
+
+# [0,0,1,1,1] using 1 swap.
+
+# The minimum is 1.
+
+# Example 2:
+
+# Input: [0,0,0,1,0]
+
+# Output: 0
+
+# Explanation:
+
+# Since there is only one 1 in the array, no swaps needed.
+
+# Example 3:
+
+# Input: [1,0,1,0,1,0,0,1,1,0,1]
+
+# Output: 3
+
+# Explanation:
+
+# One possible solution that uses 3 swaps is [0,0,0,0,0,1,1,1,1,1,1].
+
+# Note:
+
+# 1 <= data.length <= 10^5
+# 0 <= data[i] <= 1
+
+
+from typing import List
+class Solution:
+    def minSwaps(self, nums: List[int]) -> int:
+        if set(nums) == set([0]) or set(nums) == set([1]):
+            return 0
+        n_ones = sum(nums)
+        st, ed, cur_n_zeros = 0, n_ones-1, n_ones - sum(nums[:n_ones])
+        res = cur_n_zeros
+        # print(st, ed, cur_n_zeros)
+        for st in range(1, len(nums)-n_ones+1):
+            cur_n_zeros -= nums[st+n_ones-1] - nums[st-1]
+            res = min(cur_n_zeros, res)
+            if res == 0:
+                break
+            # print(st, ed, cur_n_zeros)
+        return res
