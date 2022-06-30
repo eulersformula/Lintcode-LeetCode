3e280dfaf0d5478b04db09910eb0166d8b715dee
Rinnegan <gaofeng.fdu@gmail.com>
2022-06-29 17:59:32 -0700

    Update Maximum_Subarray.py

--- Maximum_Subarray.py
+++ Maximum_Subarray.py
@@ -1,3 +1,6 @@
+# Lintcode 41//Easy
+# Leetcode 53//Medium
+
 #Given an array of integers, find a contiguous subarray which has the largest sum.
 
 #Example: Given the array [−2,2,−3,4,−1,2,1,−5,3], the contiguous subarray [4,−1,2,1] has the largest sum = 6.
@@ -17,3 +20,16 @@ class Solution:
             maxSum[1] = i if maxSum[1] < 0 else (maxSum[1] + i)
             maxSum[0] = max(maxSum[1], maxSum[0])
         return max(maxSum)
+
+# 二刷
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        res = nums[0]
+        s = nums[0]
+        min_s = min(nums[0], 0)
+        for n in nums[1:]:
+            s += n
+            res = max(s - min_s, res)
+            if s < min_s:
+                min_s = s 
+        return res
